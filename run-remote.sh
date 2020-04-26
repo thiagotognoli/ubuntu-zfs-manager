8b7513d527473b126fe09e40721055f8b9dfb1dc
Thiago Tognoli <thiagotognoli@users.noreply.github.com>
2020-04-26 15:02:32 -0300

    Update run-remote.sh

--- run-remote.sh
+++ run-remote.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY bash -c "\
-cd /tmp \
-  && sudo rm -rf /tmp/ubuntu-zfs-snapshots-manager \
+  sudo rm -rf /tmp/ubuntu-zfs-snapshots-manager \
   && sudo apt install -y git \
-  && git clone https://github.com/thiagotognoli/ubuntu-zfs-snapshots-manager.git \
-  && sudo ./ubuntu-zfs-snapshots-manager/run.sh;
+  && git clone https://github.com/thiagotognoli/ubuntu-zfs-snapshots-manager.git /tmp/ubuntu-zfs-snapshots-manager \
+  && sudo bash /tmp/ubuntu-zfs-snapshots-manager/run.sh;
   sudo rm -rf /tmp/ubuntu-zfs-snapshots-manager
 "
